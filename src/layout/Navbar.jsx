ae362ba8ca8c367ab6ab9808753a8f6d9187698a
Fardin1023 <fardinkamran915@gmail.com>
2026-06-14 22:51:18 +0600

    Update 1.0

---
+++ src/layout/Navbar.jsx
@@ -0,0 +1,27 @@
+const navLinks =[
+    {href: "#about", label:"About"},
+    {href: "#projects", label:"Projects"},
+    {href: "#experience", label:"Expereince"},
+    {href: "#testimonials", label:"Testimonials"},
+    {href: "#about", label:"About"}
+]
+export const Navbar =() =>{
+    return (
+    <header className="fixed top-0 left-0 right-0 bg-transparent py-5">
+        <nav className="container mx-auto px-6 flex items-center justify-between">
+            <a href ="#" className ="text-xl font-bold tracking-light">
+                FK<span>.</span>
+            </a>
+            <div>
+                <div>
+                    {navLinks.map((link,index) =>(
+                        <a href ={link.href} key={index}>
+                            {link.label}
+                        </a>
+                    ))}
+                </div>
+            </div>
+        </nav>
+    </header>
+    )
+};
