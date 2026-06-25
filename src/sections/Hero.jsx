0e99ad64fc2d4d6163ee6fc0156a5e2c1190d2df
Fardin1023 <fardinkamran915@gmail.com>
2026-06-25 22:59:01 +0600

    update 1.5

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,4 +1,6 @@
 import { useMemo } from "react";
+import { Button } from "@/components/Button";
+import { ArrowRight } from "lucide-react";
 
 export const Hero = () => {
   const dots = useMemo(
@@ -35,15 +37,59 @@ export const Hero = () => {
         ))}
       </div>
       {/* Content */}
-      <div>
-        <div>
+      <div className="relative z-10 max-w-6xl mx-auto px-4 sm:px-6 lg:px-8 flex flex-col md:flex-row items-center justify-between gap-8">
+        <div className="grid lg:grid-cols-2 gap-12 items-center">
           {/* Left Column -Text Content */}
-          <div>
-            <div>
-              <span className ="w-2 h-2 bg-primary rounded-full inline-block animate-pulse mr-2">
-                AI & ML Enthusiast / Full Stack Developer / Open Source
+          <div className="space-y-8">
+            <div className="animate-fade-in">
+              <span className="inline-flex items-center gap-2 px-4 py-2 rounded-full glass text-sm text-cyan-50">
+                <span className="w-2 h-2 bg-blue-500 rounded-full animate-pulse" />
+                AI & ML Enthusiast * Full Stack Developer * Open Source
                 Contributer
               </span>
+            </div>
+            {/* Headline */}
+            <div className="space-y-4 ">
+              <h1 className="text-5xl md:text-6xl font-bold text-white animate-fade-in animation-delay-200">
+                Crafting{" "}
+                <span className="text-primary-foreground glow-text">
+                  digital
+                </span>
+                <br />
+                experiences with
+                <br />
+                <span className="font-serif italic font-normal text-white">
+                  creativity and innovation.
+                </span>
+              </h1>
+              <p className="text-lg text-muted-foreground max-w-lg mt-4 animate-fade-in animation-delay-200">
+                This is Fardin Kamran, a curious and ambitious Computer Science
+                student focused on AI, software quality, and computer graphics,
+                with interests in LLMs, OpenGL development, research, and
+                technology-driven entrepreneurship.
+              </p>
+            </div>
+            {/* Call to Action Buttons */}
+            <div>
+              <button className="lg items-center bg-blue-600 text-black hover:bg-blue-900 rounded-full hover:text-white animate-fade-in animation-delay-300">
+                Contact Me <ArrowRight className="w-5 h-5"/>
+              </button>
+              <button>
+                  {/* Animated SVG border*/}
+                  <svg className="absolute left-0 top-0 w-full h-full pointer-events-auto viewBox="0 0 200 60" preserveAspectRatio="none" style={{overflow:"visible"}}
+                  >
+                    <path
+                    d="M 30,1 A 29, 29 0 0 0 1,30 L 1, 30 A 29,29 0 0 0 30,59 L"
+                    fill="none" stroke="var(--color-primary)"
+                    strokeWidth="2"
+                    strokeDasharray="400 550"
+                    strokeDashoffset="400"
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    className="download-cv-path"
+                    />
+                  </svg>
+              </button>
             </div>
           </div>
           {/* Right Column -Profile Image */}
