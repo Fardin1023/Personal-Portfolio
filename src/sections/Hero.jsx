a55d310a3103510cbb3e630f9215d6cafb7dd536
Fardin1023 <fardinkamran915@gmail.com>
2026-06-25 12:47:32 +0600

    image update

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,3 +1,11 @@
 export const Hero =() =>{
-    return <section className="relative min-h-screen flex items-center overflow-hidden"></section>
-};
+    return 
+    <section className="relative min-h-screen flex items-center overflow-hidden">
+        {/* BG image */}
+        <div>
+            <img />
+
+        </div>
+    </section>
+}
+);
